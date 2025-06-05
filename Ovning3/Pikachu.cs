99ff6bd101f707647080dd9e9cd5837fa6da9b8b
GustafssonJessica <jessica.gustafsson@live.se>
2025-06-05 12:22:46 +0200

    börjat ändra i evolve... fortsätt där!!

--- Ovning3/Pikachu.cs
+++ Ovning3/Pikachu.cs
@@ -10,6 +10,7 @@ namespace Ovning3
     {
         public string EvolveName { get; } = "Raichu"; //Raichu verkar vara slutstationen för en pikashu, så därav ingen set-funktion
 
+        public int EvolveLevel { get; private set; } = 10; //Antal nivåer som Pikachu kan levla upp med. Tänker att den bara ska kunna ändras inifrån klassen 
         public Pikachu(string name, int level, List<Attack> attacks)
             : base(name, level, attacks)
         {
@@ -17,7 +18,7 @@ namespace Ovning3
 
         public void Evolve()
         {
-            Level += 10;
+            Level = Level + EvolveLevel;
             Console.WriteLine($"Pikachu is evolving... Now it's {EvolveName}! Level {Level}!");
         }
     }
